fdd2773f0bfb8f2101020a4f8f98f355d9745917
BarreraClaudia <claudiabarrera.code@gmail.com>
2026-06-04 12:13:35 -0600

    add red styles and animations to header, add red to scores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,15 @@ import '../styles/Header.css';
 function Header() {
   return (
     <header>
-      <p className="japanese-title">鬼滅の刃</p>
-      <h1>Demon Slayer Memory Game</h1>
-      <p>Click each card once — don't repeat!</p>
+      <div className="flame-bg"></div>
+
+      <div className="header-content">
+        <p className="japanese-title">鬼滅の刃</p>
+        <h1>Demon Slayer Memory Game</h1>
+        <p>Click each card once — don't repeat!</p>
+      </div>
+
+      <div className="slash"></div>
     </header>
   );
 }
